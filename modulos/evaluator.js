98c6e4db05779ca29587e877260fda6ae3694727
Neve <alejandro.forerog@gmail.com>
2023-04-16 07:19:49 -0500

    Modules created

---
+++ modulos/evaluator.js
@@ -0,0 +1,51 @@
+
+ class Evaluator {
+
+  // Método para evaluar una señal y asignar su fondo de acuerdo a los límites establecidos
+  evaluate(signal) {
+    console.log(signal.getData());
+    if (signal.getData().length < 0) {
+        var promedio= signal.getValue();
+    }else{
+        var num = signal.getData().slice(-1);
+        console.log(num,signal.getValue());
+        promedio= Math.sqrt((Math.pow(num,2)+Math.pow(signal.getValue(),2))/2);
+    }
+    
+
+    fetch('.../info/limites.json')
+      .then(response => response.json())
+      .then(limits => {
+        if (limits[signal.getName()] == undefined) { //Verificar si no hay límites definidos para la señal
+          console.log("no existe");
+        } else {
+          console.log(promedio);
+
+          try {
+            if (
+                limits[signal.getName()].amarilla.limite_superior >= promedio &&
+                limits[signal.getName()].amarilla.limite_inferior <= promedio){
+                    console.log('amarillo');
+                    signal.setBackground("rgb(243, 216, 61,0.7)");
+            }else if(
+                limits[signal.getName()].naranja.limite_superior>=promedio &&
+                limits[signal.getName()].naranja.limite_inferior<=promedio){
+                  console.log('naranja')
+                  signal.setBackground("rgb(232, 116, 44,0.7)");
+            }else if(
+                limits[signal.getName()].roja.limite_inferior<=promedio &&
+                limits[signal.getName()].roja.limite_superior>=promedio){
+                  console.log('rojo')
+                  signal.setBackground("rgb(232, 44, 72,0.7)");
+            }else{
+                signal.setBackground("rgba(13, 148, 68, 0.7)"); 
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      })
+      .catch(error => console.error(error));
+  }
+}
+export default Evaluator;
